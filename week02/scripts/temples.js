fccf8aeaae5d38a972288f2666286677f8cd5a47
DanielR1MC <dlomasbeto2@gmail.com>
2026-07-16 08:17:52 -0400

    a

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -3,3 +3,10 @@ document.getElementById("currentyear").textContent = currentYear;
 
 document.getElementById("lastModified").innerHTML = document.lastModified;
 
+const menuButton = document.querySelector('#menu');
+const navList = document.querySelector('nav ul');
+
+menuButton.addEventListener('click', () => {
+    menuButton.classList.toggle('show');
+    navList.classList.toggle('show');
+});
