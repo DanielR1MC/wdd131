52ebc6885d29063027f42d9efd71f6792f297746
DanielR1MC <dlomasbeto2@gmail.com>
2026-07-16 01:16:13 -0400

    a

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,4 @@
 const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").textContent = currentYear;
 
-document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
+document.getElementById("lastModified").innerHTML = document.lastModified;
